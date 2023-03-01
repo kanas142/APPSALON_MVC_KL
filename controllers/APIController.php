c42a1ce6059fcd29dfd09aad3227fea05b48caa3
kanas142 <klinerosop@gmail.com>
2023-02-28 23:37:52 -0500

    Deployment de la App Configuracion remota 4

--- controllers/APIController.php
+++ controllers/APIController.php
@@ -10,7 +10,7 @@ class APIController {
     public static function index(){
         $servicios = Servicio::all();
         echo 'Hola';
-        //echo json_encode($servicios);
+        echo json_encode($servicios);
     }
 
     public static function guardar(){
