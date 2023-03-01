9429af99191d9cbb6c8a8ea117587b7cdaeeb58a
kanas142 <klinerosop@gmail.com>
2023-02-28 22:43:32 -0500

    Deployment de la App Configuracion remota 4

--- Router.php
+++ Router.php
@@ -30,7 +30,6 @@ class Router
 
         $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'] ;
         $method = $_SERVER['REQUEST_METHOD'];
-        debuguear($method);
 
         if ($method === 'GET') {
             $fn = $this->getRoutes[$currentUrl] ?? null;
